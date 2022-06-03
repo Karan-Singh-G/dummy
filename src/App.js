0b5ea7287ed7a6dea0fbabfc517d21f8aae9cc24
Karan-Singh-G <om191karan@gmail.com>
2022-06-03 18:11:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from "./component/Navbar";
+import {Route,Routes} from "./component/"
+
+function App() {
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Pages/Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Login=()=>{
+    
+    const [logincreds, setLoginCreds]=React.useState({});
+    const handeChange=(e)=>{
+        const {name,value}=e.target;
+        
+    }
+
+
+}
+
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
